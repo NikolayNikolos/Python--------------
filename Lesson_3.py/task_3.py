ea2f3307edcfce83e4346981da6b0caa48f90e47
Nikolay <nikolos50@gmail.com>
2025-04-23 22:08:06 +0300

    add fail new3

--- Lesson_3.py/task_3.py
+++ Lesson_3.py/task_3.py
@@ -5,8 +5,7 @@ to_address = Address("100123", "Москва", "Абрикосова", "1", "5")
 from_address = Address("100321", "Архангельск","Виноградная","3","6")
 
 mailing = Mailing(to_address, from_address, "2500", "TRK642")
-#mailing.sum(2500)
 print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, "
       f"{mailing.from_address.street}, {mailing.from_address.home} - {mailing.from_address.apartment} "
       f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
-      f"{mailing.to_address.home} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
+      f"{mailing.to_address.home} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.").sum(2500)
