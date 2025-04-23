d54f266767f74189db6cfa46f66d3b42cc595839
Nikolay <nikolos50@gmail.com>
2025-04-23 22:41:37 +0300

    add fail new4

--- Lesson_3.py/task_3.py
+++ Lesson_3.py/task_3.py
@@ -8,4 +8,4 @@ mailing = Mailing(to_address, from_address, "2500", "TRK642")
 print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, "
       f"{mailing.from_address.street}, {mailing.from_address.home} - {mailing.from_address.apartment} "
       f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
-      f"{mailing.to_address.home} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.").sum(2500)
+      f"{mailing.to_address.home} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
